d199255925af3379f61d2b7a7827cf90da3ff958
A <99933422+data-sandbox@users.noreply.github.com>
2023-06-14 11:31:39 -0600

    Add example of script vs. module

---
+++ main3.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+# Example to show how a Python module can also be a script
+# using the classic __name__ == "__main__" test.
+
+python3 sound/effects/echo.py
--- sound/effects/echo.py
+++ sound/effects/echo.py
@@ -4,3 +4,11 @@ def func1(a, b):
 
 def func2(a, b):
     return f'echo func2 result: {a - b}'
+
+
+if __name__ == "__main__":
+    """
+    This will run when echo.py is excecuted as script,
+    but not when it is imported as a module by other code.
+    """
+    print("Echo module executed as a script!")
